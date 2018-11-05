bbe060a28acba804ba98317f580c92099107a9a6
Quentin Prevost <quentin.prevost1495@gmail.com>
2018-11-05 10:45:39 +0100

    Initial Commit

---
+++ QuizzApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  QuizzApp
+//
+//  Created by Quentin Prevost on 05/11/2018.
+//  Copyright © 2018 Quentin Prevost. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
---
+++ QuizzAppUITests/QuizzAppUITests.swift
@@ -0,0 +1,30 @@
+//
+//  QuizzAppUITests.swift
+//  QuizzAppUITests
+//
+//  Created by Quentin Prevost on 05/11/2018.
+//  Copyright © 2018 Quentin Prevost. All rights reserved.
+//
+
+import XCTest
+
+class QuizzAppUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
